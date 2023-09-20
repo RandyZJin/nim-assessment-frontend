6bb7af618085256c4ad91e5cc4d8c2beb3c9ed50
RJ <randy.j416@yahoo.com>
2023-09-20 16:39:47 -0400

    add function to download as pdf via html2pdf.js

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -1,37 +1,47 @@
+import html2pdf from "html2pdf.js";
 import styles from "./styles/OrderConfirmation.module.css";
 
 function OrderConfirmation(props) {
   const { order } = props;
+  const downloadAsPdf = () => {
+    const receipt = document.querySelector("#receipt");
+    html2pdf(receipt);
+  };
   let orderTotal = 0;
   return (
     <div className={styles.container}>
       Thank you for your order! Your order is on the way.
-      <table>
-        <thead>
-          <tr>Order ID: {order.id}</tr>
-          <tr>Delivering to: {order.name}</tr>
-          <tr>Phone Number: {order.phone}</tr>
-          <tr>Address: {order.address}</tr>
-          <th>item</th> <th>unit price</th> <th>quantity</th> <th>subtotal</th>
-        </thead>
-        <tbody>
-          {order &&
-            order.items.map((item) => {
-              orderTotal += item.item.price * item.quantity;
-              return (
-                <tr>
-                  <td>{item.item.name}</td>
-                  <td>${item.item.price}</td>
-                  <td>{item.quantity}</td>
-                  <td>${item.item.price * item.quantity}</td>
-                </tr>
-              );
-            })}
-        </tbody>
-        <tfoot>
-          <tr>Your order total is: ${orderTotal}</tr>
-        </tfoot>
-      </table>
+      <div id="receipt">
+        <div>Order ID: {order.id}</div>
+        <div>Delivering to: {order.name}</div>
+        <div>Phone Number: {order.phone}</div>
+        <div>Address: {order.address}</div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>item</th> <th>unit price</th> <th>quantity</th>{" "}
+              <th>subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {order &&
+              order.items.map((item) => {
+                orderTotal += item.item.price * item.quantity;
+                return (
+                  <tr>
+                    <td>{item.item.name}</td>
+                    <td>${item.item.price}</td>
+                    <td>{item.quantity}</td>
+                    <td>${item.item.price * item.quantity}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        <div>Your order total is: ${orderTotal}</div>
+      </div>
+      <button onClick={() => downloadAsPdf()}>Print Receipt</button>
     </div>
   );
 }
