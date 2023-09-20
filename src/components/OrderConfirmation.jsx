1b10a792614b7ff766a47e00e228137cb49652df
RJ <randy.j416@yahoo.com>
2023-09-20 17:02:42 -0400

    add more css for order confirmation and add error text in case URL does not lead to an existing order

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -10,38 +10,44 @@ function OrderConfirmation(props) {
   let orderTotal = 0;
   return (
     <div className={styles.container}>
-      Thank you for your order! Your order is on the way.
-      <div id="receipt">
-        <div>Order ID: {order.id}</div>
-        <div>Delivering to: {order.name}</div>
-        <div>Phone Number: {order.phone}</div>
-        <div>Address: {order.address}</div>
+      {!order.id &&
+        "This order cannot be found.  Please make sure you have the right URL."}
+      {order.id && (
+        <div className={styles.receipt} id="receipt">
+          Thank you for your order! Your order is on the way.
+          <div>Order ID: {order.id}</div>
+          <div>Delivering to: {order.name}</div>
+          <div>Phone Number: {order.phone}</div>
+          <div>Address: {order.address}</div>
+          <table>
+            <thead>
+              <tr>
+                <th>item</th> <th>unit price</th> <th>quantity</th>
+                <th>subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.items &&
+                order.items.map((item) => {
+                  orderTotal += item.item.price * item.quantity;
+                  return (
+                    <tr>
+                      <td>{item.item.name}</td>
+                      <td>${item.item.price}</td>
+                      <td>{item.quantity}</td>
+                      <td>${item.item.price * item.quantity}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+          <div>Your order total is: ${orderTotal}</div>
+        </div>
+      )}
 
-        <table>
-          <thead>
-            <tr>
-              <th>item</th> <th>unit price</th> <th>quantity</th>{" "}
-              <th>subtotal</th>
-            </tr>
-          </thead>
-          <tbody>
-            {order &&
-              order.items.map((item) => {
-                orderTotal += item.item.price * item.quantity;
-                return (
-                  <tr>
-                    <td>{item.item.name}</td>
-                    <td>${item.item.price}</td>
-                    <td>{item.quantity}</td>
-                    <td>${item.item.price * item.quantity}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-        <div>Your order total is: ${orderTotal}</div>
-      </div>
-      <button onClick={() => downloadAsPdf()}>Print Receipt</button>
+      {order.id && (
+        <button onClick={() => downloadAsPdf()}>Print Receipt</button>
+      )}
     </div>
   );
 }
